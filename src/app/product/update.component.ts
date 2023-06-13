416d52de641d38d59268dd1e10af7e40fd8387e3
ivanroscuenca <ivanros@protonmail.com>
2023-06-13 21:28:23 +0200

    new changes edit for params

--- src/app/product/update.component.ts
+++ src/app/product/update.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { Product } from '../model/product';
@@ -9,38 +9,34 @@ import { Product } from '../model/product';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent {
-  
-  id!:number;
-  product!:Product;
-  constructor(private productService: ProductService,private router:Router,private activatedRoute:ActivatedRoute){}
 
-ngOnInit():void {
-  this.getProduct();
-}
+  id!: number;
+  name!: string;
+  price!: number;
+  //product!:Product;
+  constructor(private productService: ProductService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
-onUpdate():void{
-  this.productService.update(this.id,this.product).subscribe(
-    data => {
-      console.log('Product updated properly ');
-      this.router.navigate(['']);
-    },
-    err =>{
-      console.error('An error occurred:', err);
-    }
-  );
-}
+  ngOnInit(): void {
+    this.getProduct();
+  }
 
-  getProduct():void {
-    this.id = this.activatedRoute.snapshot.params['id'];
-    this.productService.detail(this.id).subscribe(
-      data =>{
-    this.product = data;
-    console.log(this.product);
-      },
-      err=>{
-        console.error('An error occurred:', err);
+  onUpdate(): void {
+    const product = new Product(this.name, this.price);
+    this.productService.update(this.id, product).subscribe(
+      data => {
+        console.log('Product updated properly ');
         this.router.navigate(['']);
+      },
+      err => {
+        console.error('An error occurred:', err);
       }
-    )
+    );
+  }
+
+  getProduct(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.name = this.activatedRoute.snapshot.params['name'];
+    this.price = this.activatedRoute.snapshot.params['price'];
+
   }
 }
